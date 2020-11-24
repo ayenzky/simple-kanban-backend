bd8c961b2aa60e6c00c7af26aacb738e2a625132
ayenzky <allen@webri.services>
2020-11-24 13:25:35 +0800

    update database

--- config/database.js
+++ config/database.js
@@ -6,6 +6,12 @@ module.exports = ({ env }) => ({
       settings: {
         client: 'sqlite',
         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        host: env("DATABASE_HOST", "localhost"),
+        port: env("DATABASE_PORT", 1337),
+        database: env("DATABASE_NAME", ""),
+        username: env("DATABASE_USERNAME", ""),
+        password: env("DATABASE_PASSWORD", ""),
+        ssl: { "rejectUnauthorized": false }
       },
       options: {
         useNullAsDefault: true,
@@ -13,3 +19,25 @@ module.exports = ({ env }) => ({
     },
   },
 });
+
+
+{
+  "defaultConnection": "default",
+  "connections": {
+    "default": {
+      "connector": "bookshelf",
+      "settings": {
+        "client": "postgres",
+        "host": "${process.env.DATABASE_HOST}",
+        "port": "${process.env.DATABASE_PORT}",
+        "database": "${process.env.DATABASE_NAME}",
+        "username": "${process.env.DATABASE_USERNAME}",
+        "password": "${process.env.DATABASE_PASSWORD}",
+        "ssl": { "rejectUnauthorized": false }
+      },
+      "options": {}
+    }
+  }
+}
+
+
